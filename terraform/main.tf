b75b8e08b08bb10511a59c54e273589f5daa03aa
yooodleee <accia25@naver.com>
2025-06-08 19:04:39 +0900

    ALB + Target Group + Listner

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,44 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_ecs_cluster" "this" {
   name = var.project_name
 }
+
+# Create ALB
+resource "aws_lb" "app_alb" {
+  name = "${var.project_name}-alb"
+  internal = false 
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.alb_sg.id]
+  subnets = [aws_subnet.public.id]
+
+  enable_deletion_protection = false 
+}
+
+# Create Target Group (where the ECS service will be connected)
+resource "aws_lb_target_group" "app_tg" {
+  name = "${var.project_name}-tg"
+  port = 3000
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200-399"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Create Listner (port 80 -> Target Group)
+resource "aws_lb_listener" "http_listner" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
