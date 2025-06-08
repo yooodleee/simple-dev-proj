69c0754b7d5e633a88579c713057c5e6370b640c
yooodleee <accia25@naver.com>
2025-06-09 00:40:07 +0900

    set security group for ssh, grafana, prometheus

--- terraform/main.tf
+++ terraform/main.tf
@@ -204,3 +204,37 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ec2_sg" {
+  name = "ec2-public-sg"
+  description = "Allow SSH, Grafana, and Prometheus"
+  vpc_id = aws_vpc.main.id
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 9090
+    to_port = 9090
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
