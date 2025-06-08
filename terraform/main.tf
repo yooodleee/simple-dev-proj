7bd8ed926f609fcafdb580509ae2796ab39f0188
yooodleee <accia25@naver.com>
2025-06-08 19:16:56 +0900

    ECS Task Definition & Task Execution Role

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,3 +96,49 @@ resource "aws_lb_listener" "http_listner" {
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "app_task" {
+  family = "${var.project_name}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  cpu = "256"
+  memory = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name = "app"
+      image = 122610481100.dkr.ecr.us-east-1.amazonaws.com/devops-vote-app
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort = 3000
+          protocol = "tcp"
+        }
+      ]
+      essential = true
+    }
+  ])
+}
+
+# Task Execution Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effact = "Allow",
+      Sid = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
